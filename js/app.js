36d6e6dec9300db70c0698c5e8dc13bf910d4f7f
Luca Nerlich <luca.nerlich@googlemail.com>
2024-03-15 12:16:20 +0100

    load css self, move highlight.js to delayed.js

--- js/app.js
+++ js/app.js
@@ -29,27 +29,26 @@ async function loadInitial(doc) {
  * @param {Document} doc The container element
  */
 async function loadLazy(doc) {
-    loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js')
-        .then(
-            () => {
-                loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/xml.min.js');
-                loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/bash.min.js');
-                loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/css.min.js');
-                loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/java.min.js');
-                loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/javascript.min.js');
-                loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/typescript.min.js');
-                loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/rust.min.js');
-                loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/python.min.js');
-                loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/json.min.js');
-                loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/yaml.min.js');
-                loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/dockerfile.min.js');
-                loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/nginx.min.js');
-                loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/scss.min.js');
-                loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/markdown.min.js');
-            }
-        )
-        .then(() => hljs.highlightAll());
-
+    // loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js')
+    //     .then(
+    //         () => {
+    //             loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/xml.min.js');
+    //             loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/bash.min.js');
+    //             loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/css.min.js');
+    //             loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/java.min.js');
+    //             loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/javascript.min.js');
+    //             loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/typescript.min.js');
+    //             loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/rust.min.js');
+    //             loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/python.min.js');
+    //             loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/json.min.js');
+    //             loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/yaml.min.js');
+    //             loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/dockerfile.min.js');
+    //             loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/nginx.min.js');
+    //             loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/scss.min.js');
+    //             loadScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/markdown.min.js');
+    //         }
+    //     )
+    //     .then(() => hljs.highlightAll());
 }
 
 /**
